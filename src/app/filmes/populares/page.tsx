1653b6e991bb7278c500815c64c4e1122aba0b3a
Valfran <valfran-goncalves@hotmail.com>
2025-07-23 18:48:51 -0300

    feat: adicionado Paginas correspondentes ao navbar

---
+++ src/app/filmes/populares/page.tsx
@@ -0,0 +1,11 @@
+import Title from "@/app/components/Title";
+
+const FilmesPopulares = () => {
+    return (
+        <>
+            <Title title="Filmes Populares" />
+        </>
+    );
+}
+
+export default FilmesPopulares;
---
+++ src/app/filmes/top-filmes/page.tsx
@@ -0,0 +1,11 @@
+import Title from "@/app/components/Title";
+
+const FilmesTopFilmes = () => {
+    return (
+        <>
+            <Title title="Filmes Top Filmes" />
+        </>
+    );
+}
+
+export default FilmesTopFilmes;
