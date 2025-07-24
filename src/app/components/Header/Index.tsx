749220cf6c4c219050ba81a8d73171d600f168ad
Valfran <valfran-goncalves@hotmail.com>
2025-07-24 17:24:52 -0300

    feat: add Header module css

--- src/app/components/Header/Index.tsx
+++ src/app/components/Header/Index.tsx
@@ -1,19 +1,22 @@
 import Link from "next/link";
+import styles from "./Header.module.css";
 
 const Header = () => {
-    return (
-        <header>
-            <Link href="/">
-            <h1>Cinelista</h1>
-            </Link>
-            <nav>
-                <Link href="/">Início</Link>
-                <Link href="/filmes/em-alta">Em Alta</Link>
-                <Link href="/filmes/populares">Populares</Link>
-                <Link href="/filmes/top-filmes">Top Filmes</Link>
-            </nav>
-        </header>
-      );
-}
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__container}>
+        <Link href="/">
+          <h1 className={styles.header__logo}>Cinelista</h1>
+        </Link>
+        <nav className={styles.header__nav}>
+          <Link href="/">Início</Link>
+          <Link href="/filmes/em-alta">Em Alta</Link>
+          <Link href="/filmes/populares">Populares</Link>
+          <Link href="/filmes/top-filmes">Top Filmes</Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
