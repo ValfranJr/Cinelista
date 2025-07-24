b708de6dfb7f80f7654e7aa17f8c7de8e4dc50ae
Valfran <valfran-goncalves@hotmail.com>
2025-07-24 17:56:01 -0300

    feat: add Card module css

--- src/app/components/Card/Index.tsx
+++ src/app/components/Card/Index.tsx
@@ -1,4 +1,5 @@
 import { Filme } from "@/types/types";
+import styles from "./Card.module.css";
 
 type Props = {
     filme: Filme
@@ -8,11 +9,11 @@ type Props = {
 const Card = ({filme}: Props) => {
     const { title, id, description, imagem } = filme;
     return (
-      <div key={id}>
-        <img src={imagem} alt={`Poster do Filme ${title}`} width={300} height={200} />
-        <div>
-          <h3>{title}</h3>
-          <p>{description}</p>
+      <div key={id} className={styles.card}>
+        <img className={styles.card__poster} src={imagem} alt={`Poster do Filme ${title}`} width={300} height={200} />
+        <div className={styles.card__info}>
+          <h3 className={styles.card__title}>{title}</h3>
+          <p className={styles.card__description}>{description}</p>
         </div>
       </div>
     );
