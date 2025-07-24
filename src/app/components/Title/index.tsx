bb307f732d40ce0959d1d6e2378900947a4b6c85
Valfran <valfran-goncalves@hotmail.com>
2025-07-24 17:32:55 -0300

    feat: add Title module css

--- src/app/components/Title/index.tsx
+++ src/app/components/Title/index.tsx
@@ -1,10 +1,12 @@
+import styles from "./Title.module.css";
+
 type Props = {
     title: string;
 }
 
 const Title = ({title}: Props) => {
     return (
-        <h2>
+        <h2 className={styles.title}>
             {title}
         </h2>
     );
