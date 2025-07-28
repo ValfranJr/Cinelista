890b380323b2650b297aafd80c2b90b9842dcd28
Valfran <valfran-goncalves@hotmail.com>
2025-07-27 21:35:59 -0300

    chore: ajustando o componente card de acordo com os novos types

--- src/app/components/Card/Index.tsx
+++ src/app/components/Card/Index.tsx
@@ -1,22 +1,32 @@
 import { Filme } from "@/types/types";
 import styles from "./Card.module.css";
+import Link from "next/link";
 
 type Props = {
-    filme: Filme
-}
+  filme: Filme;
+};
 
-
-const Card = ({filme}: Props) => {
-    const { title, id, description, imagem } = filme;
-    return (
-      <div key={id} className={styles.card}>
-        <img className={styles.card__poster} src={imagem} alt={`Poster do Filme ${title}`} width={300} height={200} />
+const Card = ({ filme }: Props) => {
+  const { title, id, overview, poster_path, vote_average } = filme;
+  const resume = overview?.length >= 256 ? `${overview?.substring(0, 253)}...` : overview;
+  return (
+    <div key={id} className={styles.card}>
+      <Link href={`/filmes/${id}`}>
+        <img
+          className={styles.card__poster}
+          src={`${process.env.NEXT_PUBLIC_TMDB_API_IMG_URL}${poster_path}`}
+          alt={`Poster do Filme ${title}`}
+          width={300}
+          height={200}
+        />
         <div className={styles.card__info}>
           <h3 className={styles.card__title}>{title}</h3>
-          <p className={styles.card__description}>{description}</p>
+          <p className={styles.card__description}>{resume}</p>
+          <p className={styles.card__rating}>Nota: {vote_average}</p>
         </div>
-      </div>
-    );
-}
+      </Link>
+    </div>
+  );
+};
 
 export default Card;
